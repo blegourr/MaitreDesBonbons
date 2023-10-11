63459fa2b97843f9bab2772b92553eab75d9e841
Blegourr <91756178+blegourr@users.noreply.github.com>
2023-10-11 07:25:32 +0000

    sendMessagePool first push

---
+++ MaitreDesBonbonsApi/authDiscordAcount/ws/sendMessagePool.js
@@ -0,0 +1,54 @@
+
+/**
+ * 
+ * @param {Array[]} poolGlobal 
+ * @param {EventEmitter} webSocketEmitter // voir pour l'importer autrement
+ * @param {Number} poolId 
+ * @param {String || Object} message 
+ */
+
+module.exports = async ({ poolGlobal, poolId, message, webSocketEmitter }) => {
+  // vérification que les données transmisent existe
+  if (!poolId || !message || !poolGlobal || !webSocketEmitter) {
+    return {
+      error: true,
+      message: 'params not found'
+    }
+  }
+
+  // récupère la pool
+  const pool = poolGlobal[poolId];
+
+  // vérifie que la pool existe
+  if (!pool) {
+    return {
+      error: true,
+      message: 'poolID not found'
+    }
+  }
+
+  // exécute le code pour tous les utilisateurs
+  pool.users.forEach((user) => {
+    // récupère le nom de l'évent
+    const ws = user.ws
+
+    if (!ws) {
+      return {
+        error: true,
+        message: `ws for user ${user.id}`
+      }
+    }
+
+    try {
+      // envoie le ws pour tous les utilisateurs
+      webSocketEmitter.emit(ws, {
+        message: message
+      })
+    } catch {
+      return {
+        error: true,
+        message: `L'event ws n'existe pas pour l'utilisateur ${user.id}`
+      }
+    }
+  })
+}
