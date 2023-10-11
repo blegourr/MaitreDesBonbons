07df43e206b991f072e000f6bf10bc865d92d7ad
Blegourr <91756178+blegourr@users.noreply.github.com>
2023-10-11 13:42:20 +0200

    finish joinPool but without test

--- MaitreDesBonbonsApi/authDiscordAcount/ws/joinPool.js
+++ MaitreDesBonbonsApi/authDiscordAcount/ws/joinPool.js
@@ -3,36 +3,34 @@
    * 
    */
 
-  // Fonction pour générer une adresse IP locale aléatoire (192.168.X.X)
-  function generateRandomIPAddress() {
-    const segment1 = 192;
-    const segment2 = 168;
-    const segment3 = Math.floor(Math.random() * 256); // Valeur aléatoire entre 0 et 255
-    const segment4 = Math.floor(Math.random() * 256); // Valeur aléatoire entre 0 et 255
-    return `${segment1}.${segment2}.${segment3}.${segment4}`;
+// Fonction pour générer une adresse IP locale aléatoire (192.168.X.X)
+function generateRandomIPAddress() {
+  const segment1 = 192;
+  const segment2 = 168;
+  const segment3 = Math.floor(Math.random() * 256); // Valeur aléatoire entre 0 et 255
+  const segment4 = Math.floor(Math.random() * 256); // Valeur aléatoire entre 0 et 255
+  return `${segment1}.${segment2}.${segment3}.${segment4}`;
+}
+
+// Fonction pour générer un mot de passe aléatoire avec des caractères spéciaux
+function generateRandomPassword(length) {
+  const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+";
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset.charAt(randomIndex);
   }
+  return password;
+}
 
-  // Fonction pour générer un mot de passe aléatoire avec des caractères spéciaux
-  function generateRandomPassword(length) {
-    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+";
-    let password = "";
-    for (let i = 0; i < length; i++) {
-      const randomIndex = Math.floor(Math.random() * charset.length);
-      password += charset.charAt(randomIndex);
-    }
-    return password;
-  }
-
-
-  
 /**
  * création de la function pour crée la pool
  * @param {Array} poolGlobal
  * @param {Number} poolId
  * @returns {Array} poolGlobal
- */  
-const poolCreation = ({poolGlobal, poolId}) => {
-  if ( !poolGlobal || !poolId ) {
+ */
+const poolCreation = ({ poolGlobal, poolId }) => {
+  if (!poolGlobal || !poolId) {
     return {
       error: true,
       message: 'params not found'
@@ -64,9 +62,9 @@ const poolCreation = ({poolGlobal, poolId}) => {
  * @param {Number} poolId
  * @param {Number} userId
  * @param {String} ws
- * @returns 
+ * @returns poolGlobal
  */
-const poolAddUser = ({poolGlobal, poolId, userId, ws}) => {
+const poolAddUser = ({ poolGlobal, poolId, userId, ws }) => {
   if (!poolGlobal || !poolId || !userId || !ws) {
     return {
       error: true,
@@ -88,7 +86,7 @@ const poolAddUser = ({poolGlobal, poolId, userId, ws}) => {
       message: 'pool not found'
     }
   }
-  
+
   // vérifie si l'utilisateur est déjà dans la pool
   if (!pool.users.some(user => user.id === userId)) {
 
@@ -105,6 +103,84 @@ const poolAddUser = ({poolGlobal, poolId, userId, ws}) => {
 
 
 /**
+ * function permetant la création de la party pour la pool adéquatte
+ * @param {Number} poolId 
+ * @param {Object} client
+ * @returns {object} db
+ */
+const creationPartyForPool = async ({ poolId, client }) => {
+  if (!client || !poolId) {
+    return {
+      error: true,
+      message: 'params not found'
+    }
+  }
+
+  if (isNaN(poolId)) {
+    return {
+      error: true,
+      message: 'poolId is not a number'
+    }
+  }
+
+  //récupère la db
+  let db = await client.getParty()
+
+  // vérifie si la party existe déjà pour la pool
+  if (db.pool.filter(pool => pool.poolID === poolId).length < 1) {
+    db.pool.push({
+      poolID: poolId,
+      players: {
+        maitreBonBon: {
+          playersID: '',
+
+        },
+        agentFbi: {
+          playersID: '',
+
+        },
+        zero: {
+          playersID: '',
+          enigme: {
+            ipMdp: {
+              finish: false,
+              ip: generateRandomIPAddress(),
+              mdp: generateRandomPassword(64),
+            },
+            fireWall: {
+              finish: false,
+              directoryListing: '/wp-content/uploads', // définie l'url à rentrer pour la faille
+              SQLInjection: " OR 1 = 1 -- - " //définir le code sql à rentrer
+            },
+            fileEncrypted: {
+              finish: false,
+              mdp: generateRandomPassword(64)
+            },
+            coordinate: {
+              finish: false,
+            }
+          }
+        }
+      },
+      software: {
+        DDOS: false,
+        webVulnerabilityScanner: false,
+        decryptFile: false,
+        webLookHtmlStructure: false,
+      },
+      attackNow: [],
+      settings: {},
+      aide: {}
+    })
+
+    await client.updateParty(db)
+  }
+
+  return db
+}
+
+
+/**
  * création de la function permettant de join la pool
  * @param {any[]} poolGlobal 
  * @param {number} userId 
@@ -113,7 +189,7 @@ const poolAddUser = ({poolGlobal, poolId, userId, ws}) => {
  * @param {string || Object} message 
  * @param {Object} client
  */
-module.exports = async ({poolGlobal, userId, poolId, ws, message, client}) => {
+module.exports = async ({ poolGlobal, userId, poolId, ws, message, client }) => {
 
   // vérification que les données nésésaire sont présente
   if (!poolGlobal || !userId || !poolId || !ws || !message || !client) {
@@ -141,111 +217,11 @@ module.exports = async ({poolGlobal, userId, poolId, ws, message, client}) => {
   })
 
   // création de la party dans la db
+  let db = creationPartyForPool({
+    poolId: poolId,
+    client: client
+  })
 
-        let db = await client.getParty()
-  
-        if (db.pool.filter(pool => pool.poolID === poolId).length >= 1) {
-          db.pool[db.pool.findIndex(pool => pool.poolID === poolId)[0]] = {
-            poolID: poolId,
-            players: {
-              maitreBonBon: {
-                playersID: '',
-  
-              },
-              agentFbi: {
-                playersID: '',
-  
-              },
-              zero: {
-                playersID: 'undefined',
-                enigme: {
-                  ipMdp: {
-                    finish: false,
-                    ip: generateRandomIPAddress(),
-                    mdp: generateRandomPassword(64),
-                  },
-                  fireWall: {
-                    finish: false,
-                    directoryListing: '/wp-content/uploads', // définie l'url à rentrer pour la faille
-                    SQLInjection: " OR 1 = 1 -- - " //définir le code sql à rentrer
-                  },
-                  fileEncrypted: {
-                    finish: false,
-                    mdp: generateRandomPassword(64)
-                  },
-                  coordinate: {
-                    finish: false,
-                  }
-                }
-              }
-            },
-            software: {
-              DDOS: false,
-              webVulnerabilityScanner: false,
-              decryptFile: false,
-              webLookHtmlStructure: false,
-            },
-            attackNow: [],
-            settings: {},
-            aide: {}
-          }
-  
-          await client.updateParty(db)      
-        } else {
-          db.pool.push({
-            poolID: poolId,
-            players: {
-              maitreBonBon: {
-                playersID: '',
-  
-              },
-              agentFbi: {
-                playersID: '',
-  
-              },
-              zero: {
-                playersID: '',
-                enigme: {
-                  ipMdp: {
-                    finish: false,
-                    ip: generateRandomIPAddress(),
-                    mdp: generateRandomPassword(64),
-                  },
-                  fireWall: {
-                    finish: false,
-                    directoryListing: '/wp-content/uploads', // définie l'url à rentrer pour la faille
-                    SQLInjection: " OR 1 = 1 -- - " //définir le code sql à rentrer
-                  },
-                  fileEncrypted: {
-                    finish: false,
-                    mdp: generateRandomPassword(64)
-                  },
-                  coordinate: {
-                    finish: false,
-                  }
-                }
-              }
-            },
-            software: {
-              DDOS: false,
-              webVulnerabilityScanner: false,
-              decryptFile: false,
-              webLookHtmlStructure: false,
-            },
-            attackNow: [],
-            settings: {},
-            aide: {}
-          })
-  
-          await client.updateParty(db)   
-        }
-  
-      if (!poolGlobal[poolId].users.some(user => user.id === userId)) {
-        poolGlobal[poolId].users.push({
-          id: userId,
-          ws: ws
-        });
-      }
-  
-      return poolGlobal[poolId].users
-    }
+
+  return poolGlobal[poolId].users
+}
