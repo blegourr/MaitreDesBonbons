94df21e5ea69d3d690c74c5b32292f5bde30b875
Blegourr <91756178+blegourr@users.noreply.github.com>
2024-02-12 20:59:42 +0100

    modifie

--- MaitreDesBonbonsApi/serveur.js
+++ MaitreDesBonbonsApi/serveur.js
@@ -1,41 +1,60 @@
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-const proxy = require('./proxy/proxy')
+const proxy = require('./proxy/proxy');
 
+// Fonction pour se connecter à la base de données
+async function connectToDatabase(uri, options) {
+    try {
+        await mongoose.connect(uri, options);
+        console.log('Connexion à la base de données réussie');
+    } catch (error) {
+        console.error('Erreur lors de la connexion à la base de données:', error);
+        throw error;
+    }
+}
 
 dotenv.config();
 
-const mongOption = {}; // Définition initiale d'un objet vide
+const DATABASE_URI = process.env.DATABASE_URI;
 
-// Configuration des options de connexion à MongoDB
-mongOption.autoIndex = false; // Ne pas construire d'index automatiquement
-mongOption.maxPoolSize = 10; // Maintenir jusqu'à 10 connexions simultanées
-mongOption.serverSelectionTimeoutMS = 5000; // Durée maximale pour sélectionner le serveur (en millisecondes)
-mongOption.socketTimeoutMS = 45000; // Durée maximale d'inactivité avant de fermer la connexion (en millisecondes)
-mongOption.family = 4; // Utiliser IPv4 (ignorer IPv6)
+const mongOption = {
+    autoIndex: false,
+    maxPoolSize: 10,
+    serverSelectionTimeoutMS: 5000,
+    socketTimeoutMS: 45000,
+    family: 4
+};
 
-// Le reste de votre code
+// Appel de la fonction pour se connecter à la base de données
+connectToDatabase(DATABASE_URI, mongOption)
+    .then(() => {
+        // Définition de la promesse mongoose.Promise à global.Promise
+        mongoose.Promise = global.Promise;
 
+        // Message de confirmation de connexion réussie
+        console.log('DB connectée');
 
-const client = {} // voir pour mettre le bot discord dedans
+        // Vérifie si une partie existe sinon la crée
+        proxy.start();
+    })
+    .catch(error => {
+        // Gestion de l'erreur de connexion à la base de données
+        console.error(`Connexion à la base de données échouée: ${error}`);
+    });
 
+// Gestion des événements de processus
+process.on('exit', code => {
+    console.log(`Le processus s'est arrêté avec le code d'erreur: ${code}!`);
+});
 
+process.on('uncaughtException', (err, origin) => {
+    console.log(`Uncaught Exception: ${err}, Origine: ${origin}`);
+});
 
-mongoose.set('strictQuery', false);
-mongoose.connect(process.env.DATABASE_URI, mongOption).then(async () => {
-  mongoose.Promise = global.Promise
-  console.log('db connecter')
+process.on('unhandledRejection', (reason, promise) => {
+    console.log(`Unhandled Rejection: ${reason}\n-----\n`, promise);
+});
 
-
-  // vérifie si une partie existe sinon la crée
-  proxy.start()
-
-}).catch((e) => {
-  //problème de connection db
-  console.error(`connection DB échoué raison: ${e}`)
-})
-
-process.on('exit', code => { console.log(`le processus s'est arrêter avec comme erreur: ${code}!`) })
-process.on('uncaughException', (err, origin) => { console.log(`uncaughException: ${err}`, `Origine: ${origin} `) })
-process.on('unhandledRejection', (reason, promise) => { console.log(`unhandledRejection: ${reason}\n-----\n`, promise) })
-process.on('warning', (...args) => { console.log(...args) })
+process.on('warning', (...args) => {
+    console.log(...args);
+});
